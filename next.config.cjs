cf5092267187f58e52b6b69e0ec0dd1e24dce14f
sam605it <sam605it@gmail.com>
2026-02-01 16:17:43 -0600

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      net: false,
-      tls: false,
-      bufferutil: false,
-      "utf-8-validate": false,
-      "supports-color": false,
-    };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        bufferutil: false,
+        'utf-8-validate': false,
+      };
+    }
     return config;
   },
 };
