087cae66734acd965c25db2cd4c6dae4b6e70b2b
sam605it <sam605it@gmail.com>
2026-02-01 13:09:06 -0600

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      bufferutil: false,
+      "utf-8-validate": false,
+      "supports-color": false
+    };
+    return config;
+  }
 };
 
-module.exports = nextConfig;
-
+export default nextConfig;
